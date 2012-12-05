f20f48483097cc5a907847d3a9699bd9e7765981
John Lancaster <john.k.lancaster@gmail.com>
2012-12-04 21:21:31 -0500

    finished third project:

--- jlank-istm6205-ae03/istm6205-ae08basic/showresume.py
+++ jlank-istm6205-ae03/istm6205-ae08basic/showresume.py
@@ -1,6 +1,7 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 import os
+import logging
 from google.appengine.ext.webapp import template
 
 import sys, string
@@ -10,8 +11,19 @@ from xml.etree.ElementTree import tostring
 
 class MainPage(webapp.RequestHandler):
     def get(self):
+      name = self.request.get('name')
+      if name == '':
+        template_values = {
+            'person': 'John Lancaster',
+            'name': 'jlancaster'
+            }
+        path = os.path.join(os.path.dirname(__file__), 'front.html')
+        return self.response.out.write(template.render(path, template_values))
+        #name = 'jlancaster'
+      Xmlname = name + '.xml'
+      logging.info(Xmlname)
       tree = ElementTree()
-      node = tree.parse(open("myresume.xml"))
+      node = tree.parse(open(Xmlname))
       personname = node.find('.//PersonName/FormattedName').text
       street = node.find('.//ContactMethod/PostalAddress/DeliveryAddress').text
       cty = node.find('.//ContactMethod/PostalAddress/Municipality').text
@@ -21,32 +33,35 @@ class MainPage(webapp.RequestHandler):
       officenum = node.find('.//ContactMethod/Telephone/Cell').text
       emailaddress = node.find('.//ContactMethod/InternetEmailAddress').text
       execsum = node.find('.//ExecutiveSummary').text
+      twitter = node.find('.//Twitter').text
       obj = node.find('.//Objective').text
 
       employers = []
       for e in node.findall('.//EmploymentHistory/EmployerOrg'):
-		employment = {}
-		employment['organization'] = e.findtext('EmployerOrgName')
-		employment['orgdomname'] = e.findtext('InternetDomainName')
-		employment['startdt'] = e.findtext('PositionHistory/StartDate/YearMonth')
-		employment['enddt'] = e.findtext('PositionHistory/EndDate/YearMonth')
-		employment['title'] = e.findtext('PositionHistory/Title')
-		employment['image'] = e.findtext('PositionHistory/Image')
-		employment['description'] = e.findall('PositionHistory/Description')
-		employers.append(employment)
+        employment = {}
+        employment['organization'] = e.findtext('EmployerOrgName')
+        employment['orgdomname'] = e.findtext('InternetDomainName')
+        employment['startdt'] = e.findtext('PositionHistory/StartDate/YearMonth')
+        employment['enddt'] = e.findtext('PositionHistory/EndDate/YearMonth')
+        employment['title'] = e.findtext('PositionHistory/Title')
+        employment['image'] = e.findtext('PositionHistory/Image')
+        employment['description'] = e.findall('PositionHistory/Description')
+        employers.append(employment)
 
       school = []
       for s in node.findall('.//EducationHistory/SchoolOrInstitution'):
-      		education = {}
-      		education['sch'] = s.findtext('School/SchoolName')
-      		education['image'] = s.findtext('School/Image')
-      		education['schoolwebsite'] = s.findtext('School/InternetDomainName')
-      		education['degree'] = s.findtext('Degree/DegreeName')
-      		education['minor'] = s.findtext('Degree/Minor')
-      		education['degreedate'] = s.findtext('Degree/DegreeDate/YearMonth')
-      		school.append(education)
+          education = {}
+          education['sch'] = s.findtext('School/SchoolName')
+          education['image'] = s.findtext('School/Image')
+          education['schoolwebsite'] = s.findtext('School/InternetDomainName')
+          education['degree'] = s.findtext('Degree/DegreeName')
+          education['minor'] = s.findtext('Degree/Minor')
+          education['degreedate'] = s.findtext('Degree/DegreeDate/YearMonth')
+          school.append(education)
 
       template_values = {
+         'name': name,
+         'twitter': twitter,
          'person': personname,
          'streetaddress': street,
          'city': cty,
